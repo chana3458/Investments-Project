08d49345a764cdb0b61d591422921b1a1a428a87
chana <cm548607430@sgur.org.il>
2025-04-29 17:27:35 +0300

    adding design

--- src/components/CustomerRegister/CustomerRegister.jsx
+++ src/components/CustomerRegister/CustomerRegister.jsx
@@ -1,53 +1,215 @@
-import { useState } from "react"
-import { useDispatch } from "react-redux"
+// import { useState } from "react"
+// import { useDispatch } from "react-redux"
 
-import { addCustomersThunk } from "../../redux/Slices/Customer/addCustomerThunk"
-import { useNavigate } from "react-router-dom"
+// import { addCustomersThunk } from "../../redux/Slices/Customer/addCustomerThunk"
+// import { useNavigate } from "react-router-dom"
 
-export const CustomerRegister=()=>{
-    const dispatch=useDispatch()
-const [newCustomer,setNewCustomer]=useState({id:"",name:"",phoneNumber:"",address:""})
-const [requestDetails,setRequestDetails]=useState({id:"",Budget:"",Risk_Level:0,Range:""})
-const [personalDetails,setPersonalDetails]=useState({kids:"",salary:"",age:"",oldestChild:"",youngestChild:"",points:0})
-const navigate=useNavigate();
-const calculate=()=>{
-
-
-    if(personalDetails.salary/personalDetails.kids>2000)
-        personalDetails.points++;
-    if(personalDetails.oldestChild-personalDetails.youngestChild/personalDetails.kids>4)
-        personalDetails.points++;
-    setRequestDetails({...requestDetails,Risk_Level:personalDetails.points})
+// export const CustomerRegister=()=>{
+//     const dispatch=useDispatch()
+// const [newCustomer,setNewCustomer]=useState({id:"",name:"",phoneNumber:"",address:""})
+// const [requestDetails,setRequestDetails]=useState({id:"",Budget:"",Risk_Level:0,Range:""})
+// const [personalDetails,setPersonalDetails]=useState({kids:"",salary:"",age:"",oldestChild:"",youngestChild:"",points:0})
+// const navigate=useNavigate();
+// const calculate=()=>{
 
 
-}
-return <div className="a">
-<button onClick={()=>navigate(`/home`)}>home</button>
-    <div>insert id</div>
-    <input type="text" onChange={(e)=>{
-        setNewCustomer({...newCustomer,id:e.target.value});
-        setRequestDetails({...requestDetails,id:e.target.value})}}/>
-    <div>insert name</div>
-    <input type="text" onChange={e=>setNewCustomer({...newCustomer,name:e.target.value})}/>
-    <div>insert phone number</div>
-    <input type="text" onChange={e=>setNewCustomer({...newCustomer,phoneNumber:e.target.value})}/>
-    <div>insert Address</div>
-    <input type="text" onChange={e=>setNewCustomer({...newCustomer,address:e.target.value})} />
-    <br></br>
+//     if(personalDetails.salary/personalDetails.kids>2000)
+//         personalDetails.points++;
+//     if(personalDetails.oldestChild-personalDetails.youngestChild/personalDetails.kids>4)
+//         personalDetails.points++;
+//     setRequestDetails({...requestDetails,Risk_Level:personalDetails.points})
 
-    <div>insert number of children</div>
-    <input type="text" onChange={e=>setPersonalDetails({...personalDetails,kids:e.target.value})}/>
-    <div>insert salary</div>
-    <input type="text" onChange={e=>setPersonalDetails({...personalDetails,salary:e.target.value})}/>
-    <div>insert age</div>
-    <input type="text" onChange={e=>setPersonalDetails({...personalDetails,age:e.target.value})}/>
-    <div>insert oldestChild</div>
-    <input type="text" onChange={e=>setPersonalDetails({...personalDetails,oldestChild:e.target.value})} />
-    <div>insert youngestChild</div>
-    <input type="text" onChange={e=>setPersonalDetails({...personalDetails,youngestChild:e.target.value})} />
-    <br></br>
-    <button onClick={async()=>{dispatch(addCustomersThunk(newCustomer));calculate()}}>ok</button>
+
+// }
+// return <div className="a">
+// <button onClick={()=>navigate(`/home`)}>home</button>
+//     <div>insert id</div>
+//     <input type="text" onChange={(e)=>{
+//         setNewCustomer({...newCustomer,id:e.target.value});
+//         setRequestDetails({...requestDetails,id:e.target.value})}}/>
+//     <div>insert name</div>
+//     <input type="text" onChange={e=>setNewCustomer({...newCustomer,name:e.target.value})}/>
+//     <div>insert phone number</div>
+//     <input type="text" onChange={e=>setNewCustomer({...newCustomer,phoneNumber:e.target.value})}/>
+//     <div>insert Address</div>
+//     <input type="text" onChange={e=>setNewCustomer({...newCustomer,address:e.target.value})} />
+//     <br></br>
+
+//     <div>insert number of children</div>
+//     <input type="text" onChange={e=>setPersonalDetails({...personalDetails,kids:e.target.value})}/>
+//     <div>insert salary</div>
+//     <input type="text" onChange={e=>setPersonalDetails({...personalDetails,salary:e.target.value})}/>
+//     <div>insert age</div>
+//     <input type="text" onChange={e=>setPersonalDetails({...personalDetails,age:e.target.value})}/>
+//     <div>insert oldestChild</div>
+//     <input type="text" onChange={e=>setPersonalDetails({...personalDetails,oldestChild:e.target.value})} />
+//     <div>insert youngestChild</div>
+//     <input type="text" onChange={e=>setPersonalDetails({...personalDetails,youngestChild:e.target.value})} />
+//     <br></br>
+//     <button onClick={async()=>{dispatch(addCustomersThunk(newCustomer));calculate()}}>ok</button>
    
-</div>
+// </div>
 
-}
+// }
+
+
+
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addCustomersThunk } from "../../redux/Slices/Customer/addCustomerThunk";
+import { useNavigate } from "react-router-dom";
+import "./CustomerRegister.css";
+
+export const CustomerRegister = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const [newCustomer, setNewCustomer] = useState({
+    id: "",
+    name: "",
+    phoneNumber: "",
+    address: ""
+  });
+  
+  const [requestDetails, setRequestDetails] = useState({
+    id: "",
+    Budget: "",
+    Risk_Level: 0,
+    Range: ""
+  });
+  
+  const [personalDetails, setPersonalDetails] = useState({
+    kids: "",
+    salary: "",
+    age: "",
+    oldestChild: "",
+    youngestChild: "",
+    points: 0
+  });
+
+  const calculate = () => {
+    let points = 0;
+    if (personalDetails.salary / personalDetails.kids > 2000)
+      points++;
+    if ((personalDetails.oldestChild - personalDetails.youngestChild) / personalDetails.kids > 4)
+      points++;
+    
+    setPersonalDetails({...personalDetails, points: points});
+    setRequestDetails({...requestDetails, Risk_Level: points});
+  };
+
+  return (
+    <div className="customer-register-container">
+      <div className="register-header">
+        <button className="home-button" onClick={() => navigate(`/home`)}>
+          <i className="fas fa-home"></i> Back to Home
+        </button>
+        <h2>Investor Registration</h2>
+      </div>
+      
+      <div className="register-form">
+        <div className="form-section">
+          <h3>Personal Information</h3>
+          <div className="form-group">
+            <label>ID Number</label>
+            <input 
+              type="text" 
+              onChange={(e) => {
+                setNewCustomer({...newCustomer, id: e.target.value});
+                setRequestDetails({...requestDetails, id: e.target.value});
+              }}
+              placeholder="Enter your ID number"
+            />
+          </div>
+          
+          <div className="form-group">
+            <label>Full Name</label>
+            <input 
+              type="text" 
+              onChange={e => setNewCustomer({...newCustomer, name: e.target.value})}
+              placeholder="Enter your full name"
+            />
+          </div>
+          
+          <div className="form-group">
+            <label>Phone Number</label>
+            <input 
+              type="text" 
+              onChange={e => setNewCustomer({...newCustomer, phoneNumber: e.target.value})}
+              placeholder="Enter your phone number"
+            />
+          </div>
+          
+          <div className="form-group">
+            <label>Address</label>
+            <input 
+              type="text" 
+              onChange={e => setNewCustomer({...newCustomer, address: e.target.value})}
+              placeholder="Enter your address"
+            />
+          </div>
+        </div>
+        
+        <div className="form-section">
+          <h3>Family Information</h3>
+          <div className="form-group">
+            <label>Number of Children</label>
+            <input 
+              type="number" 
+              onChange={e => setPersonalDetails({...personalDetails, kids: e.target.value})}
+              placeholder="Enter number of children"
+            />
+          </div>
+          
+          <div className="form-group">
+            <label>Monthly Salary</label>
+            <input 
+              type="number" 
+              onChange={e => setPersonalDetails({...personalDetails, salary: e.target.value})}
+              placeholder="Enter your monthly salary"
+            />
+          </div>
+          
+          <div className="form-group">
+            <label>Your Age</label>
+            <input 
+              type="number" 
+              onChange={e => setPersonalDetails({...personalDetails, age: e.target.value})}
+              placeholder="Enter your age"
+            />
+          </div>
+          
+          <div className="form-group">
+            <label>Age of Oldest Child</label>
+            <input 
+              type="number" 
+              onChange={e => setPersonalDetails({...personalDetails, oldestChild: e.target.value})}
+              placeholder="Enter age of oldest child"
+            />
+          </div>
+          
+          <div className="form-group">
+            <label>Age of Youngest Child</label>
+            <input 
+              type="number" 
+              onChange={e => setPersonalDetails({...personalDetails, youngestChild: e.target.value})}
+              placeholder="Enter age of youngest child"
+            />
+          </div>
+        </div>
+      </div>
+      
+      <div className="form-actions">
+        <button 
+          className="submit-button" 
+          onClick={async() => {
+            calculate();
+            dispatch(addCustomersThunk(newCustomer));
+          }}
+        >
+          Submit Registration
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,59 +1,126 @@
-//  import { Link } from "react-router-dom"
-import {  useNavigate } from "react-router-dom"
- import {  useDispatch, useSelector } from "react-redux"
-//  import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-//  import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-//  import ContactsIcon from '@mui/icons-material/Contacts';
-import "./home.css"
-// import Icon from '@mui/material/Icon';
-// import * as React from 'react';
-// import Tooltip from '@mui/material/Tooltip';
-// import IconButton from '@mui/material/IconButton';
-// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-//  import {CORS} from flask_cors
+// //  import { Link } from "react-router-dom"
+// import {  useNavigate } from "react-router-dom"
+//  import {  useDispatch, useSelector } from "react-redux"
+// //  import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+// //  import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+// //  import ContactsIcon from '@mui/icons-material/Contacts';
+// import "./home.css"
+// // import Icon from '@mui/material/Icon';
+// // import * as React from 'react';
+// // import Tooltip from '@mui/material/Tooltip';
+// // import IconButton from '@mui/material/IconButton';
+// // import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// //  import {CORS} from flask_cors
 
  
  
 
-export const Home=()=>{
-const navigate=useNavigate();
-const dispatch=useDispatch();
-//state?.customer.customers
+// export const Home=()=>{
+// const navigate=useNavigate();
+// const dispatch=useDispatch();
+// //state?.customer.customers
 
-//   function LoadingIconButton() {
-//     const [loading, setLoading] = React.useState(false);
-//     React.useEffect(() => {
-//       const timeout = setTimeout(() => {
-//         setLoading(false);
-//       }, 2000);
-//       return () => clearTimeout(timeout);
-//     });
-//     return (
-//       <Tooltip title="Click to see loading">
-//         <IconButton onClick={() => setLoading(true)} loading={loading}>
-//           <ShoppingCartIcon />
-//         </IconButton>
-//       </Tooltip>
-//     );
-//   }
+// //   function LoadingIconButton() {
+// //     const [loading, setLoading] = React.useState(false);
+// //     React.useEffect(() => {
+// //       const timeout = setTimeout(() => {
+// //         setLoading(false);
+// //       }, 2000);
+// //       return () => clearTimeout(timeout);
+// //     });
+// //     return (
+// //       <Tooltip title="Click to see loading">
+// //         <IconButton onClick={() => setLoading(true)} loading={loading}>
+// //           <ShoppingCartIcon />
+// //         </IconButton>
+// //       </Tooltip>
+// //     );
+// //   }
   
-return <div>
-<div>logo</div>
-{/* <svg data-testid="ContactsIcon"></svg> */}
-{/* <Icon>star</Icon>; */}
-<div class="mis">
-<button  onClick={()=>navigate(`/investor`)}>investor</button>
+// return <div>
+// <div>logo</div>
+// {/* <svg data-testid="ContactsIcon"></svg> */}
+// {/* <Icon>star</Icon>; */}
+// <div class="mis">
+// <button  onClick={()=>navigate(`/investor`)}>investor</button>
 
-<button  onClick={()=>navigate(`/investee`)}>investee</button>
+// <button  onClick={()=>navigate(`/investee`)}>investee</button>
 
-<button  onClick={()=>navigate(`/information`)}>information</button>
+// <button  onClick={()=>navigate(`/information`)}>information</button>
 
-<button  onClick={()=>navigate(`/manager`)}> 🤵🏻 </button></div>
-{/* <button onClick={()=>navigate(`/customerRegister`)}>add</button> */}
-{/* <button  onClick={()=>navigate(`/menu`)}>go</button> */}
-
-
-</div>
+// <button  onClick={()=>navigate(`/manager`)}> 🤵🏻 </button></div>
+// {/* <button onClick={()=>navigate(`/customerRegister`)}>add</button> */}
+// {/* <button  onClick={()=>navigate(`/menu`)}>go</button> */}
 
 
-}
+// </div>
+
+
+// }
+
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import "./home.css";
+
+export const Home = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Animation effect when component mounts
+    setIsLoaded(true);
+  }, []);
+
+  return (
+    <div className="real-estate-home">
+      <div className="hero-section">
+        <div className={`logo-container ${isLoaded ? 'loaded' : ''}`}>
+          <h1 className="logo">RealtyInvest<span>Hub</span></h1>
+        </div>
+        
+        <div className="hero-content">
+          <h2 className="hero-title">Your Gateway to Real Estate Investment Opportunities</h2>
+          <p className="hero-subtitle">Discover, Invest, and Grow Your Real Estate Portfolio</p>
+        </div>
+      </div>
+
+      <div className="investment-options">
+        <div className={`option-card ${isLoaded ? 'loaded' : ''}`}>
+          <div className="card-icon investor-icon"></div>
+          <h3>For Investors</h3>
+          <p>Find premium real estate opportunities with high ROI potential</p>
+          <button className="action-button" onClick={() => navigate(`/investor`)}>
+            Explore Investments
+          </button>
+        </div>
+
+        <div className={`option-card ${isLoaded ? 'loaded' : ''}`}>
+          <div className="card-icon investee-icon"></div>
+          <h3>For Property Owners</h3>
+          <p>List your properties and connect with potential investors</p>
+          <button className="action-button" onClick={() => navigate(`/investee`)}>
+            List Property
+          </button>
+        </div>
+
+        <div className={`option-card ${isLoaded ? 'loaded' : ''}`}>
+          <div className="card-icon info-icon"></div>
+          <h3>Market Insights</h3>
+          <p>Access the latest real estate market trends and analysis</p>
+          <button className="action-button" onClick={() => navigate(`/information`)}>
+            View Insights
+          </button>
+        </div>
+      </div>
+
+      <div className="admin-section">
+        <button className="admin-button" onClick={() => navigate(`/manager`)}>
+          <span className="admin-icon">🤵🏻</span>
+          <span className="admin-text">Management Portal</span>
+        </button>
+      </div>
+    </div>
+  );
+};
